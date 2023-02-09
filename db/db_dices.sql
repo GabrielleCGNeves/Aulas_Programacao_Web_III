371afdacd14cd1c31a162b257cdafde4a702ded8
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-02-08 22:04:50 -0300

    commit

---
+++ db/db_dices.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE DB_Dices;
+
+CREATE TABLE TB_Funcionario(
+    fun
+);
---
+++ php/cadastrar.php
@@ -0,0 +1,10 @@
+<?php
+ $nome = $_POST['formName'];
+ $email = $_POST['formEmail'];
+ $cpf = $_POST['formCpf'];
+ $telefone = $_POST['formTel'];
+
+ echo "$nome <br> $email <br>";
+
+?>
+
